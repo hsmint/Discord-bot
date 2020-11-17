e493e1dcddea5459a61ea91f5d6461227ab825ec
hsmint <hsmint.hong@gmail.com>
2020-11-18 02:03:33 +0900

    DM-1 Error in old volume showing new volume

--- commands/volume.js
+++ commands/volume.js
@@ -8,7 +8,7 @@ module.exports = {
 
     // Client not in voice channel
     if (!msg.member.voice) return msg.reply("You need to join voice channel!");
-    
+
     // Bot not in voice channel
     if (!queue) return msg.reply("I need to join in voice channel first");
 
@@ -23,11 +23,12 @@ module.exports = {
     // Change volume (0 ~ 10)
     if (args.length === 1) {
       const newVolume = Number(args[0]);
-      if (newVolume >= 0 && newVolume <= 10){
+      if (newVolume >= 0 && newVolume <= 10) {
         try {
+          const oldVolume = queue.volume;
           queue.volume = newVolume;
           queue.connection.dispatcher.setVolume(newVolume / 10);
-          msgSend(msg, 'Volume', `Change volume **${queue.volume}** to **${newVolume}**`);
+          msgSend(msg, 'Volume', `Change volume **${oldVolume}** to **${newVolume}**`);
         } catch (error) {
           msgSend(msg, 'Volume', 'Not playing any song');
         }
