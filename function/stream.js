93d3d4dc3ef52820ade2a0a50049562e3204e7c3
hsmint <hsmint.hong@gmail.com>
2020-11-20 00:05:41 +0900

    Change music play on music download finishes

--- function/stream.js
+++ function/stream.js
@@ -8,30 +8,30 @@ module.exports = {
     const queue = msg.client.queue.get(msg.guild.id);
     queue.status = true;
     const song = queue.songs[0];
-   ytdl(song.url, {filter: 'audioonly', quality: 'highestaudio'})
+    ytdl(song.url, {filter: 'audioonly', quality: 'highestaudio'})
       .pipe(fs.createWriteStream(`./assets/song${msg.guild.id}.webm`))
       .on('error', () => {
         queue.songs.shift();
         msgSend(msg, 'Play', 'There is an error on url moving to next song');
         module.exports.play(msg);
-      });
-    setTimeout(() => {
-      const dispatcher = queue.connection.play(`./assets/song${msg.guild.id}.webm`)
-        .on('finish', () => {
-          if (queue.repeat) queue.songs.push(queue.songs[0]);
-          queue.songs.shift();
-          if (!queue.songs[0]) {
-            dispatcher.end();
-            queue.songs = [];
-            queue.status = false;
-            changeActivity(msg);
-          } else {
-            changeActivity(msg);
-            return module.exports.play(msg);
-          }
-        })
-        .on('error', error => console.log(error));
-      dispatcher.setVolume(queue.volume / 10);
-    }, 2000);
+      })
+      .on('finish', () => {
+        const dispatcher = queue.connection.play(`./assets/song${msg.guild.id}.webm`)
+          .on('finish', () => {
+            if (queue.repeat) queue.songs.push(queue.songs[0]);
+            queue.songs.shift();
+            if (!queue.songs[0]) {
+              dispatcher.end();
+              queue.songs = [];
+              queue.status = false;
+              changeActivity(msg);
+            } else {
+              changeActivity(msg);
+              return module.exports.play(msg);
+            }
+          })
+          .on('error', error => console.log(error));
+        dispatcher.setVolume(queue.volume / 10);
+      })
   }
 }
