acc863572d140bbd5ebaf01deae5ebbcd685207c
hsmint <hsmint.hong@gmail.com>
2020-11-28 01:27:37 +0900

    Error handling for video downloader

--- function/stream.js
+++ function/stream.js
@@ -9,7 +9,7 @@ module.exports = {
     queue.status = true
     const song = queue.songs[0]
     try {
-      await ytdl(song.url, {filter: 'audioonly', quality: 'highestaudio'}).pipe(fs.createWriteStream(`./assets/song${msg.guild.id}.webm`))
+      ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio' }).pipe(fs.createWriteStream(`./assets/song${msg.guild.id}.webm`))
       const dispatcher = queue.connection.play(`./assets/song${msg.guild.id}.webm`)
       .on('finish', () => {
         if (queue.repeat) queue.songs.push(queue.songs[0])
@@ -24,9 +24,24 @@ module.exports = {
           return module.exports.play(msg)
         }
       })
+      .on("error", (error) => {
+        console.log(error)
+        msgSend(msg, 'Error', 'Unavailable video')
+        queue.songs.shift();
+        if (!queue.songs[0]) {
+          dispatcher.end()
+          queue.songs = []
+          queue.status = false
+          changeActivity(msg)
+        } else {
+          changeActivity(msg)
+          return module.exports.play(msg)
+        }
+      })
       dispatcher.setVolume(queue.volume / 10);
     } catch (error) {
       console.error(error)
+      msgSend(msg, 'Error', 'Unavailable video')
       queue.songs.shift()
       if (!queue.songs[0]) {
         dispatcher.end()
@@ -34,7 +49,6 @@ module.exports = {
         queue.status = false
         changeActivity(msg)
       } else {
-        msgSend(msg, 'Error', 'Cannot get video')
         return module.exports.play(msg)
       }
     }
